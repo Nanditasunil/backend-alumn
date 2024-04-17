3a6ce797dae704ebb0c0a0d72cb82cd901957819
Nanditasunil <109030620+Nanditasunil@users.noreply.github.com>
2024-04-18 01:19:17 +0530

    pushing changes for verify

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -22,7 +22,12 @@ export const sendVerificationEmail = async (user, res) => {
 
   const token = _id + uuidv4();
 
-  const link = APP_URL + "users/verify/" + _id + "/" + token;
+  const link =
+    "https://nandita-backend-alumn.onrender.com/" +
+    "users/verify/" +
+    _id +
+    "/" +
+    token;
 
   //mail options
   const mailOptions = {
@@ -84,7 +89,12 @@ export const resetPasswordLink = async (user, res) => {
   const { _id, email } = user;
 
   const token = _id + uuidv4();
-  const link = APP_URL + "users/reset-password/" + _id + "/" + token;
+  const link =
+    "https://nandita-backend-alumn.onrender.com/" +
+    "users/reset-password/" +
+    _id +
+    "/" +
+    token;
 
   //mail options
 
